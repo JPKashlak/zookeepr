b21bc62071011155fa43d3f217b8f1ea51a508c0
jpkashlak <jpkashlak@gmail.com>
2021-03-03 17:31:54 -0600

    html and js linked

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+// Instructs server to make 'public' files readily available to server.js 
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -111,6 +113,18 @@ app.post('/api/animals', (req, res) => {
   const animal = createNewAnimal(req.body, animals);
   res.json(animal)
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
